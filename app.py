401441acf042dfa39971116a7b71f4dadb5ba61f
eyo25 <eyosias.teffera@gmail.com>
2024-10-13 23:15:03 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6534d7951cc0fa84deb107a72f45dd6d36dabf423c13ece42de1854cc742507f
+size 1756
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6f8ec2477f0883fdbbf9a4004f93b8f492a5252f86744603e225df384e7bc692
+size 3674
