d23a97593406d42af86180b2b64730095dd1e349
Eyosias Wondwossen Teffera <55390323+eyo25@users.noreply.github.com>
2024-10-14 15:00:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-version https://git-lfs.github.com/spec/v1
-oid sha256:ef0b1e845eeb4d20aa1ca5afe11aabe924e377e925a779cb7481c1640bfdb0fb
-size 3582
-=======
+
 import streamlit as st
 import numpy as np
 import cv2
