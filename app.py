62388336ee8db0d7357ff880c029fe75d1627da9
Eyosias Wondwossen Teffera <55390323+eyo25@users.noreply.github.com>
2024-10-14 12:11:51 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:5dbb291a58e506f91d5700375dff24c9c36936174eb25c9deb48219f3afb7b27
+size 1808
---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:46927fe7220e233f4e7bd33108635fc4d1769cbb3035aa337c60c4a86208a605
+size 3777
